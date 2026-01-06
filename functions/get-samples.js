d11b601687cb8cfead72bded8e03ebc8d9ca0460
Mambo430 <junkmailonline@pm.me>
2026-01-06 12:05:22 -0500

    Update get-samples.js

--- functions/get-samples.js
+++ functions/get-samples.js
@@ -7,5 +7,7 @@ export async function onRequest(context) {
     .bind(`${prefix}%`)
     .all()
 
+  results.forEach(r => { r.repeaters = JSON.parse(r.repeaters); });
   return Response.json(results);
 }
+
