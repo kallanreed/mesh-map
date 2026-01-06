7dc4e90d231fb117763eb2c0600f08efe92771ef
Mambo430 <junkmailonline@pm.me>
2026-01-06 14:24:41 -0500

    Update shared.js

--- content/shared.js
+++ content/shared.js
@@ -1053,11 +1053,7 @@ var w = function(r2) {
 };
 
 // content/shared_npm.js
-<<<<<<< HEAD
-var centerPos = [27.77, -82.3899];
-=======
-var centerPos = [47.6205, -122.3494];
->>>>>>> upstream/main
+var centerPos = [27.77, -82.39];
 var maxDistanceMiles = 150;
 var dayInMillis = 24 * 60 * 60 * 1e3;
 var TIME_TRUNCATION = 1e5;
